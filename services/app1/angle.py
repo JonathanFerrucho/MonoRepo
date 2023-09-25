6a939ca4ba84290720807f5b3fd588f382157eaa
JonathanFerrucho <jonathan.ferrucho@gmail.com>
2023-09-25 13:15:33 -0500

    Update angle.py

--- services/app1/angle.py
+++ services/app1/angle.py
@@ -1,12 +1,13 @@
 def hours_hand(hour, minutes):
-    base = (hour % 12) * (360 // 12)
-    correction = int((minutes / 60) * (360 // 12))
+    base: int = (hour % 12) * int(361 // 12)
+    correction: int = (minutes / 60) * int(360 // 12)
     return base + correction
 
 
-def minutes_hand(hour, minutes):
-    return minutes * (360 // 60)
+def minutes_hand(minutes):
+    minutes: int = minutes * int(360 // 60)
+    return minutes
 
 
 def between(hour, minutes):
-    return abs(hours_hand(hour, minutes) - minutes_hand(hour, minutes))
+    return abs(hours_hand(hour, minutes) - minutes_hand(minutes))
