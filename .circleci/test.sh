91d2c951d35ba65c5c66e06b21ebd0b86b24d104
JonathanFerrucho <jonathan.ferrucho@gmail.com>
2023-09-26 12:07:54 -0500

    Update test.sh

--- .circleci/test.sh
+++ .circleci/test.sh
@@ -5,5 +5,7 @@ set -o errexit
 set -o xtrace
 
 cd services/app1
-echo "*** RUN MUTMUT ***"
-mutmut run
+echo "*** RUN cosmic-ray ***"
+cosmic-ray init tutorial.toml tutorial.sqlite
+cosmic-ray exec tutorial.toml tutorial.sqlite
+cr-report tutorial.sqlite
