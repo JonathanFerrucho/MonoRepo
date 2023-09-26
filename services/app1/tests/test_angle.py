371e0332abbee3efa2a33a18c5c59d1abf4826f6
JonathanFerrucho <jonathan.ferrucho@gmail.com>
2023-09-26 12:13:20 -0500

    Update test_angle.py

--- services/app1/tests/test_angle.py
+++ services/app1/tests/test_angle.py
@@ -1,7 +1,10 @@
+from unittest import TestCase
 import angle
 
 
-def test_twelve():
-    assert angle.between(12, 00) == 0
-    assert angle.between(1, 00) == 30
-    assert angle.between(1, 1) == 24.5
+class AngleTest(TestCase):
+
+    def test_twelve(self):
+        self.assertEqual(angle.between(12, 00), 0)
+        self.assertEqual(angle.between(1, 00), 30)
+        self.assertEqual(angle.between(1, 1), 24.5)
